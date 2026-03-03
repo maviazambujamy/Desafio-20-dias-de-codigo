f774c5f614df446466e7631e4ebc58b33213e155
Maria Vitória <mariavitoriazambuja@gmail.com>
2026-03-03 18:56:14 -0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+A = int(input())
+B = int(input())
+
+
+print(f'X = {A + B}')
