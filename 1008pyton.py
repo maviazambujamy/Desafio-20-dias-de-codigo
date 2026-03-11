18119d7faa2c061f88e33a11d445e58ab0d248d7
Maria Vitória <mariavitoriazambuja@gmail.com>
2026-03-10 21:33:30 -0300

    Add files via upload

---
+++ 1008pyton.py
@@ -0,0 +1,9 @@
+NUMBER = int(input())
+HORAS_TRABALHADAS = int(input())
+
+VALOR = float(input())
+
+SALARY = HORAS_TRABALHADAS*VALOR
+
+print(f'NUMBER = {NUMBER}')
+print(f'SALARY = U$ {SALARY:.2f}')
