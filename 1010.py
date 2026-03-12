8c57769b2529454df8253bd4a253bfc074a70ca2
Maria Vitória <mariavitoriazambuja@gmail.com>
2026-03-12 09:03:32 -0300

    Add files via upload

---
+++ 1010.py
@@ -0,0 +1,14 @@
+A, B, C = input().split()
+D, E, F = input().split()
+
+A = int(A)
+B = int(B)
+C = float(C)
+
+D = int(D)
+E = int(E)
+F = float(F)
+
+VALOR = B*C+E*F
+
+print(f'VALOR A PAGAR: R$ {VALOR:.2f}')
